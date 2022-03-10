d667577a4e86095b7ca4d4150dcd88bf4689fdcd
John Dempsey <john.dempsey@iadt.ie>
2022-03-10 11:33:20 +0000

    Some JavaScript magic

--- app/main/routes.py
+++ app/main/routes.py
@@ -123,3 +123,10 @@ def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
                                       request.form['dest_language'])})
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
