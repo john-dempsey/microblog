da8e873cd9c8c0b10f749874972a10f2d0619c7d
John Dempsey <john.dempsey@iadt.ie>
2022-03-10 15:27:53 +0000

    Application programming interfaces

--- app/__init__.py
+++ app/__init__.py
@@ -45,14 +45,15 @@ def create_app(config_class=Config):
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
-
     from app.errors import bp as errors_bp
     from app.auth import bp as auth_bp
     from app.main import bp as main_bp
+    from app.api import bp as api_bp
 
     app.register_blueprint(errors_bp)
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
